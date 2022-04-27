99df91e4b39739eb580facf8f95d2216169c044e
Gunnar Jessee <gunnarjessee@gmail.com>
2022-04-27 12:25:48 -0400

    added tankType component

---
+++ components/TankType.js
@@ -0,0 +1,77 @@
+import { LogBox, Picker, StyleSheet, Text, View } from "react-native-web";
+
+const tankTypes = {
+    ['k-tank']: {
+        'name': "K Tank",
+        'constant': "",
+        'psi': ""
+    },
+    ['d-tank']: {
+        'name': "D Tank",
+        'constant': "",
+        'psi': ""
+    },
+    ['h-tank']: {
+        'name': "H Tank",
+        'constant': "",
+        'psi': ""
+    },
+    ['m-tank']: {
+        'name': "M Tank",
+        'constant': "",
+        'psi': ""
+    }
+}
+
+
+// This is a view for react
+function TankType() {
+
+    function getTanks() {
+        const returnValue = []
+        for (const tank in tankTypes) {
+            returnValue.push(<Picker.Item label={tankTypes[tank]["name"]}
+                value={tank} key={tank}
+            />);
+        }
+        return returnValue;
+    }
+
+    return (
+        <View style={tankStyle.container}>
+            <Text style={tankStyle.title}>Tank Type</Text>
+            <Picker style={tankStyle.picker}>
+                {getTanks()}
+            </Picker>
+        </View>
+    );
+}
+
+const tankStyle = StyleSheet.create({
+    container: {
+        backgroundColor: "#004488",
+        aspectRatio: "19/9",
+        width: "30%",
+        display: "flex",
+        margin: "auto",
+        alignItems: "center",
+        marginTop: "2%",
+        paddingTop: "2%",
+        color: "#AAAAAA",
+        borderRadius: "5%"
+    },
+    
+    title: {
+        color: "#CCCCCC",
+        textAlign: "center",
+        fontSize: "1rem",
+        fontWeight: "bold"
+    },
+
+    picker: {
+        marginTop: ".5rem"
+    }
+
+});
+
+export default TankType;
